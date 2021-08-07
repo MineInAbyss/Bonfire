44940ac6a52e2f5f24eb080f2b7028cb52199fd8
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-08-07 17:28:16 -0400

    remove duplicate code

--- src/main/java/com/mineinabyss/bonfire/listeners/PlayerListener.kt
+++ src/main/java/com/mineinabyss/bonfire/listeners/PlayerListener.kt
@@ -59,11 +59,6 @@ object PlayerListener : Listener {
                 return
             }
 
-            if (player.fallDistance > BonfireConfig.data.minFallDist){
-                isCancelled = true
-                return
-            }
-
             transaction {
                 val playerBonfireUUID = Players
                     .select { Players.playerUUID eq player.uniqueId }
